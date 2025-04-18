baaa78fdfdd4ce52dea1244b9b37f2b74358b4c4
sasiska <vzuravlev20@gmail.com>
2025-04-18 12:27:56 +0300

    add: practice 6_2_2

---
+++ practice_6_2/second.py
@@ -0,0 +1,85 @@
+import random
+import math
+
+import matplotlib.pyplot as plt
+from prettytable import PrettyTable
+
+
+def f(x, n):
+    return math.sqrt(11 - n * math.sin(x) ** 2)
+
+
+def simson(x_nodes):
+    result = f(x_nodes[0], 5) + f(x_nodes[-1], 5)
+    temp_1, temp_2 = 0, 0
+    for i in range(len(x_nodes)):
+        if i % 2 == 0:
+            temp_1 += f(x_nodes[i], 5)
+        else:
+            temp_2 += f(x_nodes[i], 5)
+    result += temp_1 * 2 + temp_2 * 4
+    aboba = ((x_nodes[-1] - x_nodes[0]) / len(x_nodes)) / 3
+
+    result *= aboba
+    return result
+
+
+def print_graphic(x_values, y_values):
+    plt.plot(x_values, y_values)
+    plt.title('График функции f(x, n)')
+    plt.fill_between(x_values, y_values, color='skyblue', alpha=0.4)
+    plt.xlabel('x')
+    plt.ylabel('f(x, n)')
+    plt.grid()
+    plt.show()
+
+
+def task_1():
+    n = 5
+    x_values = [i / 10 for i in range(51)]
+    y_values = []
+    for x in x_values:
+        y_x = f(x, n)
+        y_values.append(y_x)
+
+    a = max(x_values)
+    b = max(y_values)
+    print(f"Прямоугольник размером {a} × {
+          b}, в котором целиком находится фигура\n\n")
+    return x_values, y_values
+
+
+def task_2_3_4(x_values, y_values):
+    n = 100
+    a = max(x_values)
+    b = max(y_values)
+    random_dots = [{
+        "x": random.uniform(0, a),
+        "y": random.uniform(0, b)
+    } for _ in range(n)]
+    m = 0
+    for temp_dict in random_dots:
+        if temp_dict["y"] < f(temp_dict["x"], 5) and f(temp_dict["x"], 5) is not None:
+            m += 1
+    print(m, "- рандомных точек находится внутри фигуры")
+    s = m / n * a * b
+    simpson_s = simson(x_values)
+    table = PrettyTable(["Метод Симпсона", "Примерная площадь"])
+    table.add_row([simpson_s, s])
+    print(table)
+
+    table = PrettyTable(
+        ["Относительная погрешность", "Абсолютная погрешность"])
+    table.add_row([round(abs(simpson_s - s), 3),
+                  f"{round(abs(simpson_s - s) / s * 100, 3)}%"])
+    print(table)
+
+
+def main():
+    x_values, y_values = task_1()
+    task_2_3_4(x_values, y_values)
+    print_graphic(x_values, y_values)
+
+
+if __name__ == "__main__":
+    main()
---
+++ practice_6_2/third.py
@@ -0,0 +1,65 @@
+import random
+from prettytable import PrettyTable
+import matplotlib.pyplot as plt
+import math
+
+
+def f(x, r):
+    return x + r ** 2
+
+
+def print_graphic(x_values, y_values):
+    plt.plot(x_values, y_values)
+    plt.xlim(-6, 6)
+    plt.ylim(-6, 6)
+    plt.gca().set_aspect('equal')  # Установка равного масштаба по осям
+    plt.title('Окружность')
+    plt.grid()
+    plt.show()
+
+
+def task_1():
+    n = 5
+    x_values = []
+    y_values = []
+    for angle in range(0, 361):  # от 0 до 360 градусов
+        radians = math.radians(angle)  # преобразование градусов в радианы
+        x_values.append(n * math.cos(radians))
+        y_values.append(n * math.sin(radians))
+
+    a = 2 * n
+    b = 2 * n
+    print(f"Прямоугольник размером {a} × {
+          b}, в котором целиком находится фигура\n\n")
+    return x_values, y_values
+
+
+def bobr_kurva(x_values, y_values):
+    n = 200
+    mini_x, maxi_x = min(x_values), max(x_values)
+    random_dots = [{
+        "x": random.uniform(mini_x, maxi_x),
+        "y": random.uniform(mini_x, maxi_x)
+    } for _ in range(n)]
+    m = 0
+    for temp_dict in random_dots:
+        x = temp_dict["x"]
+        y = temp_dict["y"]
+        # if (x + 5) ** 2 + (y - 5) ** 2 < 5 ** 2:
+        if x ** 2 + y ** 2 < 5 ** 2:
+            m += 1
+    pi = 4 * m / n
+    print(pi, "Примерное число pi")
+    return m
+
+
+def main():
+    x_values, y_values = task_1()
+    m = bobr_kurva(x_values, y_values)
+    print(m)
+
+    print_graphic(x_values, y_values)
+
+
+if __name__ == "__main__":
+    main()
